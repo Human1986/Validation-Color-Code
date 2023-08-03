ff1ac0b6bb9f8dfa1cc5d1b5f9aa1b46b3ea948c
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-08-03 12:12:20 +0200

    Task done

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -6,7 +6,13 @@ import java.util.regex.Pattern;
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
 
-        // Put your code here
+        if (color == null) return false;
+        if (!color.contains("#") ) return false;
+
+
+        Pattern p1 = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+        Matcher matcher = p1.matcher(color);
+        return matcher.find();
 
     }
 }
